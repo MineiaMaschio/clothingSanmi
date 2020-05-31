9d8a2cefa9c0a01e14146fd3079b8e0ea41cc4cd
douglasroyer2014 <42443398+douglasroyer2014@users.noreply.github.com>
2020-05-31 11:06:10 -0300

    ajuste nos forms do login e cadastro

--- script/login.js
+++ script/login.js
@@ -13,3 +13,31 @@ function limparCampos() {
     document.getElementById('senhaCadastro').value = '';
     event.preventDefault();
 }
+
+function validarLogin(form) {
+    if (form.email.value == '') {
+        alert('Preencha o campo email');
+        form.email.focus();
+        return false;
+    } else if (form.senha.value == '') {
+        alert('Preencha o campo senha');
+        form.senha.focus();
+        return false;
+    }
+}
+
+function validarCadastro(form){
+    if(form.nome.value === ''){
+        alert('Preencha o campo nome');
+        form.nome.focus();
+        return false;
+    }else if(form.email.value === ''){
+        alert('Preencha o campo email');
+        form.email.focus();
+        return false;
+    }else if(form.senha.value === ''){
+        alert('Preencha o campo senha');
+        form.senha.focus();
+        return false;
+    }
+}
